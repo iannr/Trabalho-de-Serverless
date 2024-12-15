f490b4e44d80d662f2768f649c29e2c1c074ec69
friche11 <vicfriche@gmail.com>
2024-12-15 18:51:43 -0300

    feat: código e docs final

--- README.md
+++ README.md
@@ -2,22 +2,24 @@
 
 Clique em Teste > Alexa Simulator e selecione o input.
 
-## Fluxo para exibir/salvar filmes
+## Fluxo de execução
 
-1. Mande alguma frase que inclua "filmes atuais", você irá receber "Tudo bem, podemos falar sobre isso o dia inteiro".
+1. Mande alguma frase que inclua "filmes atuais", você irá receber uma mensagem de Bem vindo e poderá digitar no input "ajuda" para ver as funcionalidades da Skill.
 
-   1. Isso significa que você conseguiu entrar na Skill corretamente.
-2. Agora envie "melhores filmes".
+   1. Isso significa que você conseguiu entrar na Skill corretamente e irá conseguir usá-la.
+2. Agora envie "filmes em alta".
 
    1. Nesse momento ele irá fazer uma conexão com The Movie DB para solicitar os 3 primeiros filmes em alta.
-   2. Após isso, ele irá perguntar se deseja salvar.
-3. Caso deseje salvar, diga "sim".
+   2. Ele irá te falar quais são os filmes em alta e te dar instruções para saber mais sobre algum filme ou salvar algum deles na lista de favoritos.
+3. Caso deseje salvar, diga "salvar" e logo depois o nome do filme.
 
-   1. Nesse momento o código irá fazer uma conexão com um bucket S3 e armazenar a lista de filmes demonstradas.
+   1. Nesse momento o código irá fazer uma conexão com o DynamoDB e irá armazenar o filme em uma lista.
+4. Caso deseje ver informações detalhadas de algum filme basta dizer o nome do filme.
 
-## Fluxo para imprimir filmes salvos
+   1. Nesse momento o código irá fazer uma conexão com a API do The Movie DB novamente para buscar as informações do filme solicitado e informá-las ao usuário.
+5. Caso deseje ver seus filmes salvos basta dizer "filmes salvos".
 
-1. Mande alguma frase que inclua "filmes atuais", você irá receber "Tudo bem, podemos falar sobre isso o dia inteiro".
-   1. Isso significa que você conseguiu entrar na Skill corretamente.
-2. Agora envie "assitir depois".
-   1. Nesse momento o código irá fazer conexão com o bucket S3, receber os arquivos com os filmes e imprimir.
+   1. Nesse momento o código irá consultar a lista armazenada no DynamoDB e irá retornar os filmes da lista para o usuário.
+6. Caso deseje remover um filme da lista basta dizer "remover" e logo em seguida o nome do filme.
+
+   1. Nesse momento o código irá consultar a lista armazenada no DynamoDB e irá verificar se o filme informado existe na lista. Se sim, ele remove o filme da lista.
--- lambda/index.js
+++ lambda/index.js
@@ -1,28 +1,26 @@
 const Alexa = require('ask-sdk-core');
 const axios = require('axios');
-const AWS = require('aws-sdk');
 
-const s3 = new AWS.S3();
-const bucketName = '9fce09a0-e9f7-42d6-9742-d379618c2c3b-us-east-1';
-const fileKey = 'filmes-salvos.json'
+const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzYxY2M5MGQ3MDAyMDk0NGZmOGJiODc1YjE3NTAzYiIsIm5iZiI6MTczMzk1MTAwMC4zOTkwMDAyLCJzdWIiOiI2NzU5ZmUxODlhMjBmMmZiOTg5NDk5NjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.b4f4y8OwrhX0oLxTRp7clfJVDKyYQveyE_hENEQQ1fM';
 
-const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODExN2VmMmYyZmY4YWZkZWQ1NDc0M2JiNDhhNTMxNSIsIm5iZiI6MTcxOTEwMTQ3Ni4zOTIxNjgsInN1YiI6IjYyYWI2YzNlMDNiZjg0MDA2MTk5YTU2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.99KRpkJ_-qrWQPh6dlVMVAJUXFP_HVhGva46Bwgb4TI'
+const AWS = require("aws-sdk");
+const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Tudo bem, podemos falar sobre isso o dia inteiro';
+        const speakOutput = 'Bem-vindo à Skill Filmes Atuais. Diga "ajuda" para saber como usar essa Skill.';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            .reprompt(speakOutput)
+            .reprompt('Diga "ajuda" para saber como usar essa Skill.')
             .getResponse();
     }
 };
 
-const FilmesEmCartazIntentHandler = {
+const FilmesEmAltaIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'Filmeintent';
@@ -37,26 +35,146 @@ const FilmesEmCartazIntentHandler = {
                     Authorization: 'Bearer ' + apiKey
                 }
             };
-            
+
             const response = await axios.request(options);
-            const filmes = response.data.results
-                                .map(obj => obj.title)
-                                .slice(0, 3)
-            const speakOutput = `Aqui estão alguns filmes em tendência: ${filmes.join(', ')}. \nGostaria de salvar esses filmes para assistir mais tarde?`;
-            
+            const filmes = response.data.results.map(obj => obj.title).slice(0, 3);
+
+            const speakOutput = `Os filmes em alta atualmente são: ${filmes.join(', ')}. <break time="200ms"/>
+            Se quiser saber mais sobre algum filme você pode dizer o nome dele para mais informações. <break time="200ms"/>
+            Se quiser salvar algum filme basta dizer "salvar" e logo depois falar o nome do filme.`;
+
             const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
             sessionAttributes.filmes = filmes;
             handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
-        
+
             return handlerInput.responseBuilder
                 .speak(speakOutput)
-                .reprompt('Você gostaria de salvar esses filmes?')
+                .reprompt(`Se quiser saber mais sobre algum filme você pode dizer o nome dele para mais informações. <break time="200ms"/>
+                 Se quiser salvar algum filme basta dizer "salvar" e logo depois falar o nome do filme.`)
                 .getResponse();
-                
         } catch (error) {
-            
-            const speakOutput = 'Desculpe, houve um problema ao buscar os filmes em tendência. ' + error;
+            console.error(error);
+            const speakOutput = 'Desculpe, não consegui buscar os filmes no momento.';
+
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .getResponse();
+        }
+    }
+};
+
+const SalvarFilmeIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SalvarFilmeIntent';
+    },
+    async handle(handlerInput) {
+        const filme = Alexa.getSlotValue(handlerInput.requestEnvelope, "filme");
         
+        if (!filme) {
+            const speakOutput = 'Desculpe, não consegui entender o nome do filme. Pode tentar novamente e dizer o nome completo do filme?';
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Diga o nome do filme para salvar.')
+                .getResponse();
+        }
+        
+        try {
+           
+            const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
+
+        
+            if (!persistentAttributes.filmes) {
+                persistentAttributes.filmes = [];
+            }
+
+        
+            if (!persistentAttributes.filmes.includes(filme)) {
+                persistentAttributes.filmes.push(filme);
+            }
+
+        
+            handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
+            await handlerInput.attributesManager.savePersistentAttributes();
+
+            const speakOutput = `O filme ${filme} foi salvo na sua lista.`;
+
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Deseja saber mais sobre algum filme? Diga o nome do filme.')
+                .getResponse();
+        } catch (error) {
+            console.error('Erro ao salvar o filme:', error);
+            const speakOutput = 'Desculpe, houve um problema ao salvar o filme. Tente novamente mais tarde.';
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Deseja saber mais sobre algum filme? Diga o nome do filme.')
+                .getResponse();
+        }
+    }
+};
+
+
+
+const DetalhesFilmeIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'DetalhesFilmeIntent';
+    },
+    async handle(handlerInput) {
+        const filmeSlot = handlerInput.requestEnvelope.request.intent.slots.filme.value;
+
+        if (!filmeSlot) {
+            const speakOutput = 'Não há informações disponíveis para esse filme. Diga um filme válido.';
+
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Diga o nome de um filme válido para ver suas informações.')
+                .getResponse();
+        }
+
+        try {
+            const options = {
+                method: 'GET',
+                url: `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(filmeSlot)}&language=pt-BR`,
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer ' + apiKey
+                }
+            };
+
+            const response = await axios.request(options);
+            const filme = response.data.results[0];
+
+            if (filme) {
+                
+                let dataFormatada = 'uma data não especificada';
+                if (filme.release_date) {
+                    const [ano, mes, dia] = filme.release_date.split('-');
+                    dataFormatada = `${dia}/${mes}/${ano}`;
+                }
+                
+                const speakOutput = `O filme ${filme.title} foi lançado em ${dataFormatada}. 
+                Sinopse: ${filme.overview}. <break time="200ms"/>
+                Se quiser saber mais sobre algum filme você pode dizer o nome dele para mais informações. <break time="200ms"/>
+                Se quiser salvar algum filme basta dizer "salvar" e logo depois falar o nome do filme.`;
+
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt('Deseja salvar esse filme? Diga "salvar" e logo em seguida o nome do filme')
+                    .getResponse();
+            } else {
+                const speakOutput = 'Não encontrei informações sobre esse filme. Deseja buscar outro? Diga o nome do filme em alta';
+
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt('Diga o nome de outro filme para buscar.')
+                    .getResponse();
+            }
+        } catch (error) {
+            console.error(error);
+            const speakOutput = 'Houve um problema ao buscar informações sobre o filme. Tente novamente mais tarde.';
+
             return handlerInput.responseBuilder
                 .speak(speakOutput)
                 .getResponse();
@@ -67,60 +185,36 @@ const FilmesEmCartazIntentHandler = {
 const FilmesSalvosIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'Salvosintent';
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'FilmesSalvosIntent';
     },
     async handle(handlerInput) {
         try {
-            const s3Params = {
-                Bucket: bucketName,
-                Key: fileKey
-            };
+         
+            const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
 
-            const data = await s3.getObject(s3Params).promise();
-            const filmes = JSON.parse(data.Body.toString('utf-8')).filmes;
-
-            const speakOutput = `Você salvou os seguintes filmes: ${filmes.join(', ')}`;
-
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .getResponse();
-        } catch (error) {
-            const speakOutput = 'Desculpe, houve um problema ao buscar os filmes salvos. ' + error;
-
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .getResponse();
-        }
-    }
-};
-
-const YesIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent';
-    },
-    async handle(handlerInput) {
-        try {
-            const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-            const filmes = sessionAttributes.filmes || [];
-            const speakOutput = 'Filmes salvos: ' + filmes.join(', ');
-    
-            const s3Params = {
-                Bucket: bucketName,
-                Key: fileKey,
-                Body: JSON.stringify({ filmes }),
-                ContentType: 'application/json'
-            };
-
-            await s3.putObject(s3Params).promise();
-    
-            return handlerInput.responseBuilder
-                .speak(speakOutput)
-                .getResponse();
-        } catch (error) {
-            
-            const speakOutput = 'Desculpe, houve um problema. ' + error;
         
+            if (persistentAttributes.filmes && persistentAttributes.filmes.length > 0) {
+                const filmesSalvos = persistentAttributes.filmes.join(', ');
+
+                const speakOutput = `Os filmes salvos na sua lista são: ${filmesSalvos}. 
+                Deseja saber mais sobre algum desses filmes? Basta dizer o nome do filme.`;
+
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt('Diga o nome de um dos filmes para saber mais informações.')
+                    .getResponse();
+            } else {
+                const speakOutput = 'Você ainda não salvou nenhum filme. Para salvar um filme, diga "salvar" seguido do nome do filme.';
+
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt('Diga "salvar" seguido do nome de um filme para começar.')
+                    .getResponse();
+            }
+        } catch (error) {
+            console.error('Erro ao recuperar os filmes salvos:', error);
+
+            const speakOutput = 'Houve um problema ao recuperar seus filmes salvos. Tente novamente mais tarde.';
             return handlerInput.responseBuilder
                 .speak(speakOutput)
                 .getResponse();
@@ -128,13 +222,94 @@ const YesIntentHandler = {
     }
 };
 
-const NoIntentHandler = {
+const RemoverFilmeIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NoIntent';
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RemoverFilmeIntent';
+    },
+    async handle(handlerInput) {
+        const filme = Alexa.getSlotValue(handlerInput.requestEnvelope, "filme");
+
+        if (!filme) {
+            const speakOutput = 'Desculpe, não consegui entender o nome do filme. Pode tentar novamente?';
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Diga "remover" e em seguida o nome do filme para remover com sucesso.')
+                .getResponse();
+        }
+
+        try {
+      
+            const persistentAttributes = await handlerInput.attributesManager.getPersistentAttributes();
+
+            if (!persistentAttributes.filmes || persistentAttributes.filmes.length === 0) {
+                const speakOutput = 'Você não possui filmes salvos para remover.';
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt('Diga "salvar" seguido do nome de um filme para adicionar à sua lista.')
+                    .getResponse();
+            }
+
+            const filmeIndex = persistentAttributes.filmes.indexOf(filme);
+
+            if (filmeIndex === -1) {
+                const speakOutput = `O filme ${filme} não está na sua lista de favoritos. Tente remover um filme que já tenha sido salvo.`;
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt('Diga o nome de um filme que já esteja na sua lista.')
+                    .getResponse();
+            }
+
+            persistentAttributes.filmes.splice(filmeIndex, 1);
+
+            handlerInput.attributesManager.setPersistentAttributes(persistentAttributes);
+            await handlerInput.attributesManager.savePersistentAttributes();
+
+            const speakOutput = `O filme ${filme} foi removido da sua lista de favoritos.`;
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Deseja saber mais sobre outro filme ou salvar um novo filme?')
+                .getResponse();
+        } catch (error) {
+            console.error('Erro ao remover o filme:', error);
+            const speakOutput = 'Desculpe, houve um problema ao remover o filme. Tente novamente mais tarde.';
+            return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .getResponse();
+        }
+    }
+};
+
+
+
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Entendido, não salvarei os filmes';
+        const speakOutput = `Você pode perguntar pelos filmes em alta, buscar informações sobre um filme específico ou salvar um filme em uma lista de favoritos.  <break time="200ms"/>
+        Diga o nome do filme para saber mais informações sobre <break time="50ms"/> ou diga "filmes em alta". <break time="200ms"/>
+        Se quiser já salvar um filme específico, diga "salvar" e logo em seguida o nome do filme. <break time="200ms"/>
+        Se quiser ver seus filmes salvos diga "filmes salvos" <break time="200ms"/>
+        Se quiser remover um filme da lista diga "remover" e logo em seguida o nome do filme` ;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Até mais!';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -142,13 +317,38 @@ const NoIntentHandler = {
     }
 };
 
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.error(error);
+        const speakOutput = 'Desculpe, não consegui processar sua solicitação. Tente novamente.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
+        FilmesSalvosIntentHandler,
         LaunchRequestHandler,
-        FilmesEmCartazIntentHandler,
-        YesIntentHandler,
-        NoIntentHandler,
-        FilmesSalvosIntentHandler
-        )
-    .withCustomUserAgent('sample/hello-world/v1.2')
+        FilmesEmAltaIntentHandler,
+        SalvarFilmeIntentHandler,
+        DetalhesFilmeIntentHandler,
+        RemoverFilmeIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .withPersistenceAdapter(
+        new ddbAdapter.DynamoDbPersistenceAdapter({
+            tableName: process.env.DYNAMODB_PERSISTENCE_TABLE_NAME,
+            createTable: false,
+            dynamoDBClient: new AWS.DynamoDB({apiVersion: 'latest', region: process.env.DYNAMODB_PERSISTENCE_REGION})
+        })
+    )
     .lambda();
